541e6d8bc362e317b2b596e0e46e3783bfb0c9ff
LeuisKen <jiaxun.wjx@alipay.com>
2016-07-18 11:07:36 +0800

    update:搭建开发环境

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <div>45121</div>
+  }
+}
+
+export default App
---
+++ webpack.config.prod.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'app': './src/index.js'
+  },
+  output: {
+    path: path.resolve('./dist/static'),
+    filename: '[name].js',
+    publicPath: '/static/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+};
