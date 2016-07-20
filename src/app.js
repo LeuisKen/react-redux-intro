ef36d26b5ccaa79c0878ee5076bedefb47b1f564
LeuisKen <jiaxun.wjx@alipay.com>
2016-07-20 12:31:16 +0800

    update:加入antd并写了简单demo，引入less loader

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import { Table } from 'antd'
+
+const columns = [{
+  title: '名称',
+  dataIndex: 'name',
+  key: 'name'
+}]
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      dataSource: []
+    }
+    let self = this
+    fetch(`https://api.github.com/users/facebook/repos`)
+      .then(res => res.json())
+      .then(res => self.setState({
+        dataSource: res
+      }))
   }
   render() {
-    return <div>45121</div>
+    return (
+      <Table columns={columns} dataSource={this.state.dataSource}/>
+    )
   }
 }
 
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var rucksack = require('rucksack-css');
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry: {
@@ -19,7 +22,23 @@ module.exports = {
         test: /\.jsx?/,
         exclude: /node_modules/,
         loader: 'babel'
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('css!postcss!less-loader'),
       }
     ]
-  }
+  },
+  postcss: [
+    rucksack(),
+    autoprefixer({
+      browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8'],
+    })
+  ],
+  plugins: [
+    new ExtractTextPlugin('[name].css', {
+      disable: false,
+      allChunks: true,
+    })
+  ]
 };
